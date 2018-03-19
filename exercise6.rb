b0cb0c1019ebb25211dd0d5a6d89bfe35769522a
Hasan Naseem <hn92@live.com>
2018-03-19 15:21:15 -0400

    excercise6.2

--- exercise6.rb
+++ exercise6.rb
@@ -3,21 +3,27 @@
 
 counter = 0
 response = "home"
-while counter >= 0 && response != "go home"
+energy = 10
+while counter >= 0 && response != "go home" && energy > 0
 
 puts "Would you like to walk or run?"
 response = gets.chomp
 
-  if response == "walk"
+  if response == "walk" && energy > 0
     counter += 1
-    puts "Distance from home is #{counter}km"
+    energy += 2
+    puts "Distance from home is #{counter}km and your energy is #{energy}"
 
-  elsif response == "run"
+  elsif response == "run" && energy > 0
     counter += 5
-    puts "Distance from home is #{counter}km"
+    energy -= 5
+    puts "Distance from home is #{counter}km and your energy is #{energy}"
+
+  elsif energy <= 0
+    puts "your energy is down! you cant work out anymore"
 
   else
-    puts "Distance from home is #{counter}km"
+    puts "Distance from home is #{counter}km and your energy is #{energy}"
   end
 
 end
